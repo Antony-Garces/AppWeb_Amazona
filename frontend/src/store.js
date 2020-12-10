26ff8902c15db234d3fc30a7d80c594fad36a72c
Antony Garces <angarpas382@hotmail.com>
2020-12-10 00:14:30 -0500

    Implement Signin Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,19 +5,26 @@ import {
  productListReducer, 
  productDetailsReducer 
 } from "./reducers/productReducers";
+import { userSigninReducer } from "./reducers/userReducers";
 
 
 const initialState = {
- cart:{
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
+  cart:{
   cartItems: localStorage.getItem('cartItems')
-   ? JSON.parse(localStorage.getItem('cartItems'))
-   : [],
- }
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : [],
+  }
 };
 const reducer = combineReducers({
  productList: productListReducer,
  productDetails: productDetailsReducer,
- cart: cartReducer
+ cart: cartReducer,
+ userSignin: userSigninReducer
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
